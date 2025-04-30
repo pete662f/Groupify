affeb157d03d541c86bd0f3b8fe9c4e7feaa8da5
Peter Kaae Thorhauge <peter.kaae.thorhauge@gmail.com>
2025-04-30 15:38:33 +0200

    Added Greedy Grouping with Swap Optimization

--- Groupify/Models/Domain/Insight.cs
+++ Groupify/Models/Domain/Insight.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
 using Groupify.Models.Identity;
 namespace Groupify.Models.Domain;
 
@@ -12,6 +13,8 @@ public class Insight
     public int Green { get; set; }
     public int Blue { get; set; }
     public int Yellow { get; set; }
+
+    public Vector<float> ToVector() => new Vector<float>(new float[]{ Red, Green, Blue, Yellow });
 
     public virtual ApplicationUser ApplicationUser { get; set; } = null!;
 }
