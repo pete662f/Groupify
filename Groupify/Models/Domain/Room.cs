ffdaa078d3439bf0dac6ccf81bc9b7a8e19d8b36
Peter Kaae Thorhauge <peter.kaae.thorhauge@gmail.com>
2025-05-09 14:05:43 +0200

    Remove unused user management methods from Room model for cleaner code

--- Groupify/Models/Domain/Room.cs
+++ Groupify/Models/Domain/Room.cs
@@ -18,33 +18,4 @@ public class Room
     public ICollection<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
     
     public ICollection<Group> Groups { get; set; } = new List<Group>();
-    
-    /*public void AddUser(ApplicationUser user)
-    {
-        if (Users.Contains(user))
-            throw new InvalidOperationException("Student already in room");
-        Users.Add(user);
-        // student.Rooms.Add(this);
-    }
-    
-    public void RemoveUser(ApplicationUser user)
-    {
-        if (!Users.Contains(user))
-            throw new InvalidOperationException("Student not in room");
-        Users.Remove(user);
-        // student.Rooms.Remove(this);
-    }
-    
-    public void CreateGroup(int groupSize)
-    {
-        if (groupSize <= 0) throw new ArgumentException("Group size must be greater than 0");
-        var all = Users.ToList();
-        for (int i = 0; i < all.Count; i += groupSize)
-        {
-            var group = new Group { Room = this };
-            foreach (var user in all.Skip(i).Take(groupSize))
-                group.Users.Add(user);
-            Groups.Add(group);
-        }
-    }*/
 }
